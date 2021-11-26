3df093b6d0d529889df7d5ee1faa6b9328edd68f
ehsan <ehsansaraee6570@yahoo.com>
2021-11-26 22:44:16 +0330

    Navbar Added

---
+++ Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+   return (
+      <header>
+         <h1>Logo</h1>
+         <nav>
+            <ul>
+               <li>1</li>
+               <li>2</li>
+               <li>3</li>
+            </ul>
+         </nav>
+      </header>
+   );
+};
+
+export default Navbar;
